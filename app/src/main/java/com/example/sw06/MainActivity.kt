e85ea66f93eba4d01ef591a6adc7c12946cbf30c
Valon Salihu <valon.salihu@stud.hslu.ch>
2020-03-29 14:54:31 +0200

    ex. 2

--- app/src/main/java/com/example/sw06/MainActivity.kt
+++ app/src/main/java/com/example/sw06/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.sw06
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.annotation.MainThread
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val WAITING_TIME_MILIS: Long = 7000
+    private var demoThread: Thread = createDemoThread()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,7 +21,11 @@ class MainActivity : AppCompatActivity() {
             freeze7Seconds(it)
         }
 
+        val btn_demo = findViewById<Button>(R.id.btn_thread_start)
 
+        btn_demo.setOnClickListener {
+            startDemoThread(it)
+        }
     }
 
     @Suppress("UNUSED_PARAMETER")
@@ -27,9 +33,31 @@ class MainActivity : AppCompatActivity() {
         try {
             Thread.sleep(WAITING_TIME_MILIS)
         } catch (e: InterruptedException) {
-
+            //TODO
         }
     }
 
+    private fun startDemoThread(view: View?) {
+        if (!demoThread.isAlive) {
+            demoThread = createDemoThread()
+            demoThread.start()
+            btn_thread_start.text = "[Demo-Thread läuft...]"
+        } else {
+            Toast.makeText(this, "DemoThread läuft schon!", Toast.LENGTH_LONG)
+        }
+    }
 
+    private fun createDemoThread(): Thread {
+        return object : Thread("HSLUDemoThread") {
+            override fun run() = try {
+                sleep(WAITING_TIME_MILIS)
+                runOnUiThread {
+                    btn_thread_start.text = "Demo-Thread starten"
+                    Toast.makeText(this@MainActivity, "Demo Thread beendet!", Toast.LENGTH_LONG)
+                }
+            } catch (e: InterruptedException) {
+                //TODO
+            }
+        }
+    }
 }
